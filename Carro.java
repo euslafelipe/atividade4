c5ab8c1c15700d0ccc80a708ee1ace197659d622
euslafelipe <felipeferraz2332@gmail.com>
2022-11-02 13:10:33 -0300

    RESOLVIDO

--- Carro.java
+++ Carro.java
@@ -16,34 +16,34 @@ public class Carro {
         velocidade = 0;
         
     }
+    void printStatus(){
+        System.out.println("Carro [marca="+this.marca+", modelo="+this.modelo+", velocidade="+this.velocidade+"]");
+    }
+
     void acelerar(int valor){
-        this.velocidade += valor;
+        int soma = this.velocidade+valor;
+        if(soma>=200){
+            soma=200;
 
             
         }
+        System.out.println(this.velocidade+" + "+valor+" > "+soma);
+        this.velocidade=soma;
+    }
         
 
            
         
     
 
-    void frear(int desacelerar){
-        this.velocidade -= desacelerar;
-        if (velocidade <=0) {
-            System.out.println("acelere ou o carro ira parar");
-            
+    void frear(int remvel){
+        int subt =this.velocidade-remvel;
+        if(subt<=0){
+            subt=0;
         }
-        
+        System.out.println(this.velocidade+" - "+remvel+" > "+subt);
+        this.velocidade=subt;
     }
 
-
-    @Override
-    public String toString() {
-        return "carro [modelo=" + modelo + ", marca=" + marca + ", velocidade=" + velocidade"]";
-    }
-
-    public  void printstatus() {
-        System.out.println(toString());
-    }
     
 }
